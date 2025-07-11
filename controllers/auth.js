1f2d5b7c931c1580d97762d3aea8ceb980bcf0c8
HowardHarrison <peterthureain@gmail.com>
2025-07-11 12:50:34 +0630

    [Add] uuid in naming image in user account

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,47 +4,52 @@ import User from "../models/User.js";
 
 /* REGISTER USER */
 export const register = async (req, res) => {
-    try {
-        const {
-            firstName,
-            lastName,
-            email,
-            password
-        } = req.body;
-        const profileImage = req.file ? req.file.filename : "profile.jpg";
-      
-        const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password, salt);
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password
+    } = req.body;
+    const profileImage = req.file ? `assets/${req.file.filename}` : "assets/profile.jpg";
 
-        const newUser = new User({
-            firstName,
-            lastName,
-            email,
-            password: passwordHash,
-            profileImage,
-          });
-          const savedUser = await newUser.save();
-          res.status(201).json(savedUser);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-        console.log('err',err);
-    }
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      profileImage,
+    });
+    const savedUser = await newUser.save();
+    const token = jwt.sign({ id: savedUser._id }, process.env.JWT_SECRET);
+    const user = savedUser.toObject();
+    delete user.password;
+
+    res.status(201).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log('err', err);
+  }
 }
 
 /* LOGGING IN */
 export const login = async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const user = await User.findOne({ email: email });
-      if (!user) return res.status(400).json({ msg: "User does not exist. " });
-  
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
-  
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-      delete user.password;
-      res.status(200).json({ token, user });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const userToReturn = user.toObject();
+    delete userToReturn.password;
+    res.status(200).json({ token, user: userToReturn });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
--- middleware/multer.js
+++ middleware/multer.js
@@ -1,11 +1,13 @@
 import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    const uniqueName = `${uuidv4()}${file.originalname}`;
+    cb(null, uniqueName);
   },
 });
 
