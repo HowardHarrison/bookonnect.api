d2e602534c6668fafc207cdf3332a43224d975f6
HowardHarrison <peterthureain@gmail.com>
2026-01-18 21:59:45 +0630

    [Add] admin login controller & route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import Admin from "../models/Admin.js";
 
 /* REGISTER USER */
 export const register = async (req, res) => {
@@ -53,3 +54,22 @@ export const login = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+// Admin Log In
+export const adminLogin = async (req, res) => {
+  try{
+    const {email, password} = req.body;
+    const admin = await  Admin.findOne({email: email});
+    if (!admin) return res.status(400).json({ msg: "Admin does not exist."});
+
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) return res.status(400).json({msg: "Invalid credentials"});
+
+    const token = jwt.sign({ id: admin._id}, process.env.JWT_SECRET);
+    const adminToReturn = admin.toObject();
+    delete adminToReturn.password;
+    res.status(200).json({ token, user: adminToReturn });
+  } catch (err) {
+    res.status(500).json({ error: err.message});
+  }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { login, register } from "../controllers/auth.js";
+import { adminLogin, login, register } from "../controllers/auth.js";
 import upload from "../middleware/multer.js";
 
 const router = express.Router();
 
+//user
 router.post("/login", login);
 router.post("/register", upload.single('profileImage'), register);
+//admin
+router.post("/admin/login", adminLogin);
+
 export default router;
